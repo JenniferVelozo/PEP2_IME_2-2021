b4398d9008cfb3c09b89faaa42465ac553030331
Jennifer <jennifer.velozo@usach.cl>
2022-01-21 20:00:06 -0300

    Verificación de condiciones

--- Forma-11.R
+++ Forma-11.R
@@ -1,6 +1,49 @@
+library ( tidyverse )
+library ( ggpubr )
+library ( ez )
+
 # Lectura del archivo 
 
 dir <- "E:/IME/PEP2_IME_2-2021"
 base <- "Datos PEP 2.csv"
 arch <- file.path(dir,base)
 datos<-read.csv2(arch, fileEncoding = "UTF-8")
+
+# Se piensa utilizar la prueba de ANOVA para más de dos muestras 
+# correlacionadas, dado que cada soldado es evaluado por los distintos 
+# oficiales evaluadores, y se pide determinar si 
+# los niveles de exigencia con que los distintos oficiales evaluadores 
+# califican a los snowtroopers son similares o distintos. 
+
+# Hipótesis a contrastar
+# H0: La media de la evaluación realizada por los oficiales es la misma para todos.
+# HA: La media de la evaluación realizada por los oficiales es distinta para al menos uno. 
+
+# Verificación de condiciones
+
+# 1.- La escala con que se mide la variable dependiente tiene las propiedades de una escala de intervalos iguales.
+# Esta condición se cumple debido a que la evaluacion de cada oficial se encuentra entre un rango de valores de 500 y 700, 
+# siendo así una variable continua, dado que la diferencia que existe entre 700 y 500 es la misma entre 600 y 400
+
+# 2.- Las mediciones son independientes al interior de cada grupo, como se ha forzado a un 
+# joven analista de datos a evaluar el resultado del entrenamiento de los nuevos datos se puede 
+# asumir que las mediciones son independientes debido al título que tiene el joven y tiene experiencia
+# en realizar este tipo de pruebas.
+
+# 3.- Al realizar un gráfico Q-Q para cada grupo se aperecian algunos valores atípicos en los extremos superiores e inferiores,
+# por lo que se procede a ser más cautelosos y realizar la prueba con un nivel de exigencia más alto
+# alfa = 0.01
+
+# Llevar data frame a formato largo .
+datos <- datos %>% pivot_longer (c("eval_instructor", "eval_capitan", "eval_comandante","eval_general"),
+                                 names_to = "evaluador", values_to = "exigencia")
+
+datos[["evaluador"]] <- factor ( datos [["evaluador"]])
+
+# Comprobción de normalidad.
+g <- ggqqplot (datos , x = "exigencia", y = "evaluador", color = "evaluador")
+g <- g + facet_wrap (~ evaluador)
+g <- g + rremove ("x.ticks") + rremove ("x.text")
+g <- g + rremove ("y.ticks") + rremove ("y.text")
+g <- g + rremove ("axis.title")
+print ( g )
